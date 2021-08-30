bb5072b9523c89e9adde3b24487de43ba62475ed
marville001 <mwangimartin1904@gmail.com>
2021-08-30 12:40:50 +0300

    Updated db

--- db.js
+++ db.js
@@ -7,6 +7,15 @@ try {
   //   database: process.env.MYSQL_DB,
   // });
   console.log("Connected to database successfully");
+
+  db = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.MYSQL_DB,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  })
 } catch (error) {
   throw new Error(error)
 }
