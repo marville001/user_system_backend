92301bf019d47a292c060b20d1eec3b8a6388ae1
marville001 <mwangimartin1904@gmail.com>
2021-08-30 17:22:18 +0300

    Added login route

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const generateToken = require("../helpers/generateJWT");
 
 const { v4: uuidv4 } = require("uuid");
 const { validateUsers: validate } = require("../helpers/validateUser");
-const { create, getAll, getOne } = require("../services/users");
+const { create, getAll, getOne, login } = require("../services/users");
 
 const getUser = async (req, res) => {
   const { id } = req.params;
@@ -34,17 +36,31 @@ const createUser = async (req, res) => {
   body.password = encPass;
   body.id = uuidv4();
 
-  create(body, (err, result) => {
+  create(body, (err, user) => {
     if (err) return res.status(400).send({ success: false, err });
 
-    res.send({ success: true, message: result });
+    const token = generateToken(user.email, user.id)
+    res.send({ success: true, user, token });
   });
 };
 
 const loginUser = async(req, res)=>{
-	const users = await db.query("SELECT * FROM users")
-	res.send({users})
-
+  const schema = Joi.object().keys({
+    email: Joi.string().required(),
+    password:Joi.string().min(8).required()
+  })
+	const { error } = schema.validate(req.body);
+  if (error)
+    return res
+      .status(400)
+      .send({ success: false, message: error.details[0].message });
+  
+  login(req.body, (err, user)=>{
+    if (err) return res.status(400).send({ success: false, message: err });
+    const token = generateToken(user.email, user.id)
+    res.send({ success: true, user, token });
+  })
+  
 }
 
 module.exports = {
--- services/users.js
+++ services/users.js
@@ -1,14 +1,18 @@
 const db = require('../config/db')
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
+
+
 module.exports = {
   create: async (data, callback) => {
     const result = await db.query("SELECT * FROM users where email = ?", [data.email])
     if (result[0]) return callback("User exists");
 
     const { id, firstname, lastname, email, password, gender, age, contact } = data;
-    const exec = await db.query("INSERT INTO users (_id, firstname, lastname, email, password,contact ,gender, age) VALUES(?,?,?,?,?,?,?)", [id, firstname, lastname, email, password, contact, gender, age]);
+    const exec = await db.query("INSERT INTO users (_id, firstname, lastname, email, password,contact ,gender, age) VALUES(?,?,?,?,?,?,?,?)", [id, firstname, lastname, email, password, contact, gender, age]);
     if (exec.affectedRows) {
       const user = await db.query("SELECT * FROM users where email = ?", [data.email])
+
       callback(null, _.pick(user[0], [
         "_id",
         "firstname",
@@ -23,9 +27,7 @@ module.exports = {
 
   getAll: async (callback) => {
     try {
-
-
-      const users = await db.query("SELECT * FROM USERS");
+      const users = await db.query("SELECT * FROM users");
       callback(null, _.map(users, _.partialRight(_.pick, [
         "_id",
         "firstname",
@@ -33,28 +35,57 @@ module.exports = {
         "email",
         "gender",
         "age",
+        "contact"
       ])));
-    } catch (error) {
-
-      if (err) return callback(err);
+    } catch (err) {
+      callback(err);
     }
 
   },
 
-  getOne: (id, callback) => {
-    db.execute("SELECT * FROM USERS WHERE _id = ?",
-      [id], (err, results) => {
-        if (err) return callback(err);
-        callback(null, _.pick(results[0], [
-          "_id",
-          "firstname",
-          "lastname",
-          "email",
-          "gender",
-          "age",
-        ]));
-      });
+  getOne: async (id, callback) => {
+    try {
+      const results = await db.query("SELECT * FROM users WHERE _id = ?", [id]);
+      callback(null, _.pick(results[0], [
+        "_id",
+        "firstname",
+        "lastname",
+        "email",
+        "gender",
+        "age",
+        "contact"
+      ]));
+    } catch (err) {
+      callback(err);
+    }
+  },
+
+  login: async (data, callback) => {
+    try {
+      const results = await db.query("SELECT * FROM users WHERE email = ?", [data.email]);
+      const user = results[0];
+      if (!user) return callback("User does not exist")
+
+      const validPassword = await bcrypt.compare(data.password, user.password);
+      if (!validPassword) return callback("Invalid Details")
+
+      callback(null, _.pick(user, [
+        "_id",
+        "firstname",
+        "lastname",
+        "email",
+        "gender",
+        "age",
+        "contact"
+      ]));
+
+    } catch (err) {
+      console.log(JSON.stringify());
+      callback(err.message);
+    }
   }
+
+
 
 
 };
