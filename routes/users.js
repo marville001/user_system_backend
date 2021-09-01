f714d27b017158ea11fd52d7bcbf6b41c1606023
marville001 <mwangimartin1904@gmail.com>
2021-09-01 11:56:27 +0300

    Added delete user route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router();
-const { getUser, getUsers, createUser, loginUser } = require("../controllers/users")
+const { getUser, getUsers, createUser, loginUser, deleteUser } = require("../controllers/users")
 const auth = require("../middleware/auth")
 
 router.get('/', auth, getUsers);
 router.get('/:id', auth, getUser);
-router.post('/', createUser);
+router.post('/',auth, createUser);
 router.post('/login', loginUser);
+router.delete('/', auth, deleteUser);
 
 module.exports = router;
