02819e8c63bf2bb8b38d2507dfb92ac0d531176e
marville001 <mwangimartin1904@gmail.com>
2021-09-01 11:51:32 +0300

    Updates

--- routes/projects.js
+++ routes/projects.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router();
-const { createProject, getProject, getProjects } = require("../controllers/projects")
+const { createProject, getProject, getProjects, deleteProject } = require("../controllers/projects")
 const auth = require("../middleware/auth")
 
 router.get('/', auth, getProjects);
 router.get('/:id',auth, getProject);
 router.post('/', auth, createProject);
+router.delete('/', auth, deleteProject);
 
 module.exports = router;
