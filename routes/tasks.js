91cd2f7d7fde840fe13eaeb95a5f11d71e9d59db
marville001 <mwangimartin1904@gmail.com>
2021-09-01 12:51:39 +0300

    Added delete task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router();
-const { createTask, getTask, getTasks } = require("../controllers/projectTasks")
+const { createTask, getTask, getTasks, deleteTask } = require("../controllers/projectTasks")
 const auth = require("../middleware/auth")
 
 router.get('/', auth,  getTasks);
 router.get('/:id', auth,  getTask);
 router.post('/', auth,  createTask);
+router.delete('/', auth,  deleteTask);
 
 
 module.exports = router;
